69f23ae698f947e823f4fb15556f3830c9a676a8
MatDiaz <mattydiazz@gmail.com>
2018-12-11 14:06:13 -0500

    Update Mobile-Branch

--- user_estudiante/practice/assets/js/MobileIdentifier.js
+++ user_estudiante/practice/assets/js/MobileIdentifier.js
@@ -1,1 +1,13 @@
- 
+ function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
+if(isMobileDevice())
+{
+	console.log("Es Celuco");
+	console.log(screen.orientation.type);
+}
+else
+{
+	console.log("No es celuco");
+}
